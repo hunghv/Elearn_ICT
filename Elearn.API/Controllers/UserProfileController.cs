307fb8574686f2ef1b499a6c4956634cdc21e1f2
Mr.Pat <anhptgc00043@gmail.com>
2018-04-10 01:12:00 +0700

    SignUp Error

---
+++ Elearn.API/Controllers/UserProfileController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Service.Services.Interfaces;
+using Service.ViewModels.Request;
+
+namespace Elearn.API.Controllers
+{
+    [RoutePrefix("api/UserProfile")]
+    public class UserProfileController : ApiController
+    {
+        #region Contructor
+
+        private readonly IUserProfileServices _userProfileServices;
+
+        public UserProfileController(IUserProfileServices userProfileServices)
+        {
+            _userProfileServices = userProfileServices;
+        }
+
+        #endregion
+
+        #region UserProfile
+
+        [Route("SignUp")]
+        [HttpPost]
+        public HttpResponseMessage SignUpUserProfile(LoginRequest request)
+        {
+            try
+            {
+                var result = _userProfileServices.SignUp(request);
+                if (result)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, true);
+                }
+                return Request.CreateResponse(HttpStatusCode.NotModified, false);
+            }
+            catch (Exception exception)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, exception.Message);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Service/Services/UserProfileServices.cs
@@ -0,0 +1,49 @@
+﻿using Elearn.Data.Entities;
+using Elearn.Data.Repository.Interfaces;
+using Service.Common;
+using Service.Services.Interfaces;
+using Service.ViewModels.Request;
+
+namespace Service.Services
+{
+    class UserProfileServices : IUserProfileServices
+    {
+        #region Declare Property
+
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public UserProfileServices(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        #endregion
+
+        #region Public Method
+
+        //Sign up
+        public bool SignUp(LoginRequest request)
+        {
+            var emails = _userProfileRepository.GetSingleNoneDeleted(x => x.Email == request.Email);
+            if (emails != null)
+            {
+                return false;
+            }
+            //add new
+            var newUserProfile = new UserProfile
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                Password = CryptoMd5.Encode(request.Password),
+                CreatedBy = Constants.GetUserId(),
+                ModifiedBy = Constants.GetUserId(),
+                CreatedDate = Constants.GetDateNow(),
+                ModifiedDate = Constants.GetDateNow()
+            };
+            _userProfileRepository.Add(newUserProfile);
+            return _userProfileRepository.Commit();
+        }
+
+        #endregion
+    }
+}
